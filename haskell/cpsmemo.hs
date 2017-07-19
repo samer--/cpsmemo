d2082eca9feafd9e9a974ae4bdc15006b41acaf3
samer-jukedeck <samer@jukedeck.com>
2017-07-19 14:14:19 +0100

    Stop using ListT, allow any MonadPlus instead

--- haskell/cpsmemo.hs
+++ haskell/cpsmemo.hs
@@ -8,40 +8,35 @@ import Prelude hiding (foldl, foldr)
 import Control.Monad.Cont
 import Control.Monad.ST
 import Control.Monad.Ref
-import Control.Monad.Trans.List
 import Control.Monad.Fix
 import Data.Foldable
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
--- class (Monad m, MonadPlus n) => MonadMemoTable m n where
---   runM  :: n a -> m [a]
---   memoM :: (Ord a, Ord b) => (a -> n b) -> m (m [(a,[b])], a -> n b)
-
-type NDC r s = ContT r (ListT (ST s)) -- nondeterministic continuation monad
-type NDCK r s a b =  a -> NDC r s b   -- Kleilsi arrow of NDC monad
+type NDC s n r = ContT (n r) (ST s)    -- nondeterministic continuation monad
+type NDCK s n r a b = a -> NDC s n r b -- Kleilsi arrow of NDC monad ... SNRAB.
 type Table a b = [(a, Set.Set b)]
 
-instance MonadPlus (NDC r s) where
-  mzero = ContT {runContT= \k -> mzero}
-  mplus f g = ContT {runContT= \k -> runContT f k `mplus` runContT g k}
+instance MonadPlus n => MonadPlus (NDC s n r) where
+  mzero = ContT {runContT= \_ -> return mzero}
+  mplus f g = ContT {runContT= \k -> liftM2 mplus (runContT f k) (runContT g k)}
 
-run :: NDC a s a -> ST s [a]
-run m = runListT (runContT m return)
+run :: MonadPlus n => NDC s n a a -> ST s (n a)
+run m = runContT m (return . return)
 
-memo' :: (Ord a, Ord b) => (NDCK r s a b) -> ST s (ST s (Table a b), NDCK r s a b)
+memo' :: (MonadPlus n, Ord a, Ord b) => (NDCK s n r a b) -> ST s (ST s (Table a b), NDCK s n r a b)
 memo' f = do
   loc <- newRef Map.empty
   let sanitize (x,(s,_)) = (x,s)
   let feed x table k = do
-      let update e t = lift (lift (writeRef loc (Map.insert x e t)))
+      let update e t = writeRef loc (Map.insert x e t)
       let consumer (res,conts) = do
           update (res, k:conts) table
           foldr' (mplus . k) mzero res
       let producer = do
           update (Set.empty, [k]) table
           y <- f x
-          table' <- lift (lift (readRef loc))
+          table' <- readRef loc
           let Just (res,conts) = Map.lookup x table'
           if Set.member y res then mzero
           else update (Set.insert y res, conts) table' >>
@@ -50,7 +45,7 @@ memo' f = do
   return (readRef loc >>= return . map sanitize . Map.assocs,
           \x -> readRef loc >>= callCC . feed x)
 
-memo :: (Ord a, Ord b) => (NDCK r s a b) -> ST s (NDCK r s a b)
+memo :: (MonadPlus n, Ord a, Ord b) => (NDCK s n r a b) -> ST s (NDCK s n r a b)
 memo = fmap snd . memo'
 
 -- Fast Fibonnaci function
@@ -60,26 +55,34 @@ ffib n = runST $ mdo
   run (fib n)
 
 
+-- Parser combinators ----------------------------------------
+
 infixl 7 *>
 infixl 6 <|>
 
-type Parser r s a = [a] -> NDC r s [a]
+type Parser s n r a = NDCK s n r [a] [a]
+type ParserGen s n r a = ST s (Parser s n r a)
+type ParserGen' s n r a = ST s (ST s (Table [a] [a]), Parser s n r a)
 
-(*>), (<|>) :: Parser r s a -> Parser r s a -> Parser r s a 
-epsilon :: Parser r s a
-term :: Eq a => a -> Parser r s a
-
-( *> ) f g xs = f xs >>= g
-( <|> ) f g xs = (f xs) `mplus` (g xs)
-epsilon xs = return xs 
+(*>)  f g xs = f xs >>= g
+(<|>) f g xs = (f xs) `mplus` (g xs)
 term x (y:ys) | x==y = return ys
 term x _ = mzero
+epsilon xs = return xs 
 
-parse :: (forall s. ST s (Parser [t] s t)) -> [t] -> [[t]]
+parse :: (forall s. ParserGen s [] [t] t) -> [t] -> [[t]]
 parse p xs = runST $ p >>= run . ($ xs)
 
--- Some grammars to play with -------------------------------
+parse' :: (forall s. ParserGen' s [] [t] t) -> [t] -> ([[t]], Table [t] [t])
+parse' gen xs = runST $ do
+  (getter, p) <- gen
+  result <- run (p xs)
+  table <- getter
+  return (result, table)
 
+-- -- Some grammars to play with -------------------------------
+
+johnson :: MonadPlus n => ParserGen s n r String
 johnson = mdo
   v   <- return $ term "likes" <|> term "knows"
   pn  <- return $ term "Kim" <|> term "Sandy"
@@ -90,16 +93,13 @@ johnson = mdo
   s   <- memo $ np *> vp
   return s
 
+a :: MonadPlus n => Parser s n r Char
 a = term 'a'
 
-s :: ST s (Parser r s Char)
-s = mdo
-  s <- return $ a *> s *> s <|> epsilon
-  return s
+s, sm, sml, smml :: MonadPlus n => ParserGen s n r Char
 
-sm = mdo
-  sm <- memo $ a *> sm *> sm <|> epsilon
-  return sm
+s   = mfix (\s -> return $ a *> s *> s <|> epsilon)
+sm  = mfix (\s -> memo   $ a *> s *> s <|> epsilon)
 
 sml = mdo
   sml <- memo $ sml *> sml *> a <|> epsilon
@@ -110,14 +110,21 @@ smml = mdo
   aux  <- memo $ smml *> a
   return smml
 
+-- with table getter
+sm' :: MonadPlus n => ParserGen' s n r Char
+sm' = mdo
+  (getter, sm) <- memo' $ a *> sm *> sm <|> epsilon
+  return (getter, sm)
+
+tomita_sentence i = "nvdn" ++ Prelude.concat (replicate i "pdn")
+
+tomita1, tomita2 :: MonadPlus n => ParserGen s n r Char
 tomita1 = mdo
   np <- memo $ term 'n' <|> term 'd' *> term 'n' <|> np *> pp
   pp <- memo $ term 'p' *> np
   vp <- memo $ term 'v' *> np <|> vp *> pp
   s  <- memo $ np *> vp
   return s
-
-tomita_sentence i = "nvdn" ++ Prelude.concat (replicate i "pdn")
 
 tomita2 = let t = term in mdo
   advm <- memo $ t 'a' *> advm <|> t 'a' <|> advm *> t 'c' *> advm
